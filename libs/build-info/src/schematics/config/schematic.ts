b8ba587cfc4d9f5c14a585b2445387b9cbdc535c
Merzough Münker <mmuenker@digitaix.com>
2021-06-10 19:12:50 +0200

    fix: add pack target update

--- libs/build-info/src/schematics/config/schematic.ts
+++ libs/build-info/src/schematics/config/schematic.ts
@@ -1,13 +1,12 @@
-import {
-  chain,
-  Rule
-} from '@angular-devkit/schematics';
+import { chain, externalSchematic, Rule, SchematicsException } from '@angular-devkit/schematics';
 import { updateWorkspace } from '@nrwl/workspace';
 import { ConfigSchema } from './schema';
 
 export default function(options: ConfigSchema): Rule {
 
   return async () => {
+
+    let hasPackTarget: boolean | null = null;
 
     return chain([
       updateWorkspace((workspace) => {
@@ -24,14 +23,28 @@ export default function(options: ConfigSchema): Rule {
         } else {
 
           project.targets.add({
-            name:    'build-info',
+            name: 'build-info',
             builder: '@rxap/plugin-build-info:build',
             options: {}
           });
 
         }
 
-      })
+        hasPackTarget = project.targets.has('pack');
+
+      }),
+      () => {
+        if (hasPackTarget === null) {
+          throw new SchematicsException('It is unclear if the project has a the target "pack"');
+        }
+        if (hasPackTarget) {
+          console.log('Project has pack target');
+          return externalSchematic('@rxap/plugin-pack', 'add-target', {
+            project: options.project,
+            target: `${options.project}:build-info`
+          });
+        }
+      }
     ]);
 
   };
