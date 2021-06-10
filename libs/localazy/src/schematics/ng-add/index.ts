bbbe52ba5abee027db77130faa26daa2ddc49c72
Merzough Münker <mmuenker@digitaix.com>
2021-06-10 16:14:21 +0200

    fix: streamline the ng-add schematic

--- libs/localazy/src/schematics/ng-add/index.ts
+++ libs/localazy/src/schematics/ng-add/index.ts
@@ -1,20 +1,17 @@
-import { chain, Rule } from '@angular-devkit/schematics';
-import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schematics/tasks';
-import { AddPackageJsonDevDependency, AddPackageJsonScript } from '@rxap/schematics-utilities';
+import { chain, Rule, schematic } from '@angular-devkit/schematics';
+import { AddPackageJsonScript } from '@rxap/schematics-utilities';
 import { NgAddSchema } from './schema';
 
 export default function(options: NgAddSchema): Rule {
   return async () => {
 
     return chain([
-      AddPackageJsonDevDependency('@localazy/cli'),
       AddPackageJsonScript('localazy', 'localazy'),
-      (_, context) => {
-        const installTaskId = context.addTask(new NodePackageInstallTask());
+      () => {
         if (options.project) {
-          context.addTask(new RunSchematicTask('config', {
-            project: options.project,
-          }), [ installTaskId ]);
+          return schematic('config', {
+            project: options.project
+          });
         } else {
           console.log('Default project not defined. Add the localazy target to a project with: ng g @rxap/plugin-localazy:config [project]');
         }
