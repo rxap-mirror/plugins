c004fc21351245ef943fc59e345a238c1944b9bb
Merzough Münker <mmuenker@digitaix.com>
2021-06-29 15:17:22 +0200

    fix: install @localazy/cli on ng-add

--- libs/localazy/src/schematics/ng-add/index.ts
+++ libs/localazy/src/schematics/ng-add/index.ts
@@ -1,9 +1,10 @@
 import { chain, Rule, schematic } from '@angular-devkit/schematics';
-import { InstallPeerDependencies } from '@rxap/schematics-utilities';
+import { AddPackageJsonDevDependency, InstallPeerDependencies } from '@rxap/schematics-utilities';
 import { NgAddSchema } from './schema';
 
 export default function(options: NgAddSchema): Rule {
   return chain([
+    AddPackageJsonDevDependency('@localazy/cli'),
     InstallPeerDependencies(),
     () => {
       if (options.project) {
