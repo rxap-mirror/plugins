d5957fcb4c5297930c7964cdf8a67a6cf752d5cb
Merzough Münker <mmuenker@digitaix.com>
2022-12-13 09:52:51 +0100

    feat(docker): add push option

--- libs/docker/src/builders/build/builder.ts
+++ libs/docker/src/builders/build/builder.ts
@@ -6,6 +6,7 @@ import {
 import { BuildBuilderSchema } from './schema';
 import { json } from '@angular-devkit/core';
 import { DockerBuild } from './docker-build';
+import { DockerPush } from './docker-push';
 import { DockerLogin } from './docker-login';
 import { join } from 'path';
 
@@ -97,12 +98,24 @@ export class Builder {
 
     console.log(`start docker build for ${this.options.dockerfile}`);
 
-    const result = await dockerBuild.executor(
+    let result = await dockerBuild.executor(
       this.options.command,
       this.options.context,
       destinationList,
       this.options.dockerfile,
     );
+
+    if (Number(result)) {
+      return { success: false };
+    }
+
+    if (this.options.push) {
+      const dockerPush = new DockerPush(this.context.logger as any);
+      result = await dockerPush.executor(
+        this.options.command,
+        destinationList
+      );
+    }
 
     return { success: !Number(result) };
 
